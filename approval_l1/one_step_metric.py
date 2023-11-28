fb8ef2f9500a6d9a4e03226169539fcdcdf3ede1
Janix4000 <janix4000@gmail.com>
2023-11-28 21:09:51 +0100

    Missing index

--- approval_l1/one_step_metric.py
+++ approval_l1/one_step_metric.py
@@ -163,7 +163,7 @@ def main():
 
             with open(os.path.join(results_dir, "reference-report.csv"), 'a') as report_out:
                 run_experiment(approvalwise_vectors, reference_approvalwise_vectors, num_new_instances,
-                               reference_algorithm, report_out, results_dir)
+                               reference_algorithm, trial_idx, report_out, results_dir)
         else:
             reference_approvalwise_vectors = generate_reference(
                 approvalwise_vectors, num_new_instances, algorithm, sys.stdout)
