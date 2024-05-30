cce4739d3a8982dd012f937ad6e3f553899feda1
Janix4000 <janix4000@gmail.com>
2024-05-30 22:47:39 +0200

    Generating reference yields report file

--- approval_l1/generate_reference.py
+++ approval_l1/generate_reference.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import time
+
+import pandas as pd
 
 from scripts.gurobi import gurobi_ilp
 from scripts.approvalwise_vector import dump_to_text_file, load_from_text_file
@@ -14,11 +17,8 @@ def main():
     parser.add_argument('--file', type=str,
                         help='File consisting elections', required=True)
     parser.add_argument('--num_instances', type=int, default=12)
-
-    # Parse the command line arguments
     args = parser.parse_args()
 
-    # Get the values from the command line arguments
     filepath = args.file
     num_instances = args.num_instances
 
@@ -28,16 +28,24 @@ def main():
     dirpath = os.path.dirname(filepath)
     ref_filepath = os.path.join(dirpath, 'reference.txt')
 
+    report_rows = []
+
     last_distance = None
     reference_app_vecs = []
+    report_filepath = os.path.join(dirpath, 'reference_report.csv')
     for i in range(num_instances):
+        start_time = time.process_time()
         app_vec, dist = gurobi_ilp(
             approvalwise_vectors, max_dist=last_distance)
+        dt = time.process_time() - start_time
         reference_app_vecs.append(app_vec)
         approvalwise_vectors.append(app_vec)
         with open(ref_filepath, 'w') as file:
             dump_to_text_file(reference_app_vecs, file)
-        print(f'{i},{dist}')
+        print(f'{i},{dist},{dt}')
+        report_rows.append([i, dist, dt])
+        pd.DataFrame(report_rows, columns=['i', 'distance', 'dt']).to_csv(
+            report_filepath, index=False)
 
 
 if __name__ == "__main__":
