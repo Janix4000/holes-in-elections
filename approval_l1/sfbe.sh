e94972c1c2f733b821ed9db90d3ff6a97cde740c
Janix4000 <janix4000@gmail.com>
2024-06-13 12:48:43 +0200

    Remove pairs

--- approval_l1/sfbe.sh
+++ approval_l1/sfbe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for fam in noise truncated_urn euclidean resampling; do
-    for algo in basin_hopping basin_hopping_random pair greedy_dp; do
+    for algo in basin_hopping basin_hopping_random greedy_dp; do
         python3 space_filling_big_elections.py --family $fam --algorithm $algo &
     done
 done
